3ff8bb1709272f6e0e2070d52abcc03064d37b47
Mehran Tarif <mrmntf@riseup.net>
2020-12-10 11:40:34 +0330

    006

--- myscript.js
+++ myscript.js
@@ -1,3 +1,10 @@
+
+// this code allow us to print some text in page!
 document.write("Hello World from myscript file!");
+/*
+	i writed here some
+	comments!
+*/
 console.log("Here is My Console!");
-alert("How Are You?");
+
+// alert("How Are You?");
