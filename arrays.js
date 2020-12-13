7b47d9a5f7d2941d60372e106b9082d706aa3408
Mehran Tarif <mrmntf@riseup.net>
2020-12-13 11:43:26 +0330

    files

--- arrays.js
+++ arrays.js
@@ -1,8 +1,15 @@
-// let students = ["Mehran", 'Ali', "Amir"];
+let students = ["Mehran", 'Ali', "Amir", 'Reza', 'Hasan'];
 
-// console.log(students.toString())
-// console.log(students.join(' ** '))
+// students.push("Sima")
+// students.push("Sara")
+// students.pop()
+// students.pop()
+// students.shift()
+// students.shift()
+// students.unshift("Sima")
+// students.unshift("Sara")
+// students.splice(1, 2, 'Sima', 'Sara')
 
+let new_students = students.slice(1, 5)
 
-let students = "Mehran, Ali, Amir";
-console.log(typeof students.split(', '))
+console.log(new_students)
