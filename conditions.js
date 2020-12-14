ab0c2960476a74423c7bbac3be38c3df11d509d6
Mehran Tarif <mrmntf@riseup.net>
2020-12-14 12:06:37 +0330

    files

--- conditions.js
+++ conditions.js
@@ -1,14 +1,32 @@
 // let access = false;
-let mehran = [8, false]
+// let mehran = [8, false]
 
 // access = (age >= 18) ? true : false;
 
 // console.log(access)
 
-if (mehran[0] > 15 && mehran[1]) {
-	console.log("Your point are great!")
-} else if (mehran[0] > 10 || mehran[1]) {
-	console.log("Yout point is not too bad")
-} else {
-	console.log("Your point is bad.")
+// if (mehran[0] > 15 && mehran[1]) {
+// 	console.log("Your point are great!")
+// } else if (mehran[0] > 10 || mehran[1]) {
+// 	console.log("Yout point is not too bad")
+// } else {
+// 	console.log("Your point is bad.")
+// }
+
+
+let age = '18';
+
+switch (age) {
+	case '18':
+		console.log('Age is 18')
+		break;
+	case 15:
+		console.log('Age is 15')
+		break;
+	case 12:
+		console.log('Age is 12')
+		break;
+	default:
+		console.log("age is unknown.")
+		break;
 }
