cb6d3bbc268e80e9dfcfc1015f69dc3fbdaab52d
Mehran Tarif <mrmntf@riseup.net>
2020-12-14 11:34:46 +0330

    files

---
+++ conditions.js
@@ -0,0 +1,12 @@
+// let access = false;
+let age = 15;
+
+// access = (age >= 18) ? true : false;
+
+// console.log(access)
+
+if (age >= 18) {
+	console.log("Welcome to my site.")
+} else {
+	console.log("You are under 18 and can not see my site!")
+}
