027b053da07d632fdfd3dac71b3ca48bc418d700
Mehran Tarif <mrmntf@riseup.net>
2020-12-15 12:34:45 +0330

    files

--- loops.js
+++ loops.js
@@ -1,16 +1,21 @@
-let n = 1000000007;
-
-let isPrime = true;
-
-let i = 2;
-while (i < parseInt(n ** 0.5) + 1) {
-	if (n % i == 0) {
-		isPrime = false;
-		console.log(i)
-		break;
+function isPrime(n) {
+	let isPrime = true;
+	let i = 2;
+	while (i < parseInt(n ** 0.5) + 1) {
+		if (n % i == 0) {
+			isPrime = false;
+			// console.log(i)
+			break;
+		}
+		i++;
 	}
-	i++;
+	return isPrime;
 }
 
-console.log(n, isPrime)
+let a = 1000000007;
+let b = 1000000008;
+let c = 1000000021;
 
+console.log(a, isPrime(a))
+console.log(b, isPrime(b))
+console.log(c, isPrime(c))
