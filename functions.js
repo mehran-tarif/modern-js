75b315c22ff45e067caee0897055ed30e02f775d
Mehran Tarif <mrmntf@riseup.net>
2020-12-15 12:43:08 +0330

    files

--- functions.js
+++ functions.js
@@ -1,11 +1,19 @@
-function say_hello () {
-	console.log("Hello!")
-	console.log("Good Morning!")
-}
+// const say_hello = function () {
+// 	console.log("Hello!")
+// 	console.log("Good Morning!")
+// };
 
-const say_hello = function () {
-	console.log("Hello!")
-	console.log("Good Morning!")
-};
+// const addWith5 = a => {
+// 	return a + 5
+// };
 
-say_hello();
+// console.log(addWith5(2))
+
+
+let nums = [1,2,4,7,8,5]
+nums.forEach(n => console.log(n))
+
+// let nums = [1,2,4,7,8,5]
+// nums.forEach(function (n) {
+// 	console.log(n)
+// })
