b31eaa5c952baf9b59178064a028aceba05ca143
Mehran Tarif <mrmntf@riseup.net>
2020-12-15 11:35:14 +0330

    files

--- loops.js
+++ loops.js
@@ -1,7 +1,14 @@
-let students = ["Mehran", "Sima", "Reza", "Hasan", "Ali", "Sara"]
-
 let i = 0;
-while (i < students.length) {
-	console.log(`Hello ${students[i]}`);
+while (i < 20) {
 	i++;
+
+	if (i == 10) {
+		// continue;
+		break;
+	}
+
+	if (i % 2 == 0) {
+		console.log(i);
+	}
 }
+
