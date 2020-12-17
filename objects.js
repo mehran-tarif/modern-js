6683d112069520812b05505110b92cd52035c417
Mehran Tarif <mrmntf@riseup.net>
2020-12-17 11:50:57 +0330

    files

--- objects.js
+++ objects.js
@@ -1,25 +1,24 @@
-let student = {
-	firstname: "Mehran",
-	lastname: "Tarif",
-	age: 23,
-	fields: ["programming", "nursing"],
-	status: true,
-
-	fullname() {
-		return `${this.firstname} ${this.lastname}`
+let articles = [
+	{
+		title: "My First Article.",
+		description: "Bacon ipsum dolor amet beef ribs fatback ribeye tri-tip. Pork belly sirloin turkey ground round, tri-tip pork loin hamburger salami shankle ham hock t-bone. Frankfurter filet mignon strip steak pork chop chislic bacon jowl leberkas tongue ball tip bresaola turkey jerky shank landjaeger. Fatback spare ribs short ribs shankle ham hock pastrami.",
+		views: 30,
 	},
-
-	sayHello() {
-		console.log(`Hello ${this.fullname()}!`)
+	{
+		title: "My Second Article.",
+		description: "Alcatra tail tenderloin andouille prosciutto, chuck salami frankfurter venison shoulder capicola tri-tip turducken corned beef. Jowl cow ball tip jerky, hamburger short loin turkey pig fatback ribeye landjaeger meatloaf. Salami alcatra sausage flank spare ribs pig fatback chicken cupim burgdoggen swine tenderloin chuck. Jowl bacon buffalo cupim venison shank spare ribs ground round corned beef frankfurter chuck chicken beef ribs. Ball tip drumstick corned beef filet mignon turkey kielbasa.",
+		views: 30,
 	},
+	{
+		title: "My 3th Article.",
+		description: "Ham sausage short loin pork chop kevin beef ribs tail pig turducken andouille brisket buffalo spare ribs boudin. Sirloin andouille pork chop pancetta, bresaola ham shoulder leberkas fatback pork belly meatball chicken. Strip steak frankfurter venison beef pork belly boudin, drumstick landjaeger ham hock ball tip brisket. Bacon tail tongue landjaeger buffalo, flank andouille leberkas picanha kevin sausage.",
+		views: 30,
+	},
+]
 
-	showFields() {
-		this.fields.forEach((field, index) => {
-			console.log(`${index + 1}. ${field}`)
-		});
-	}
-}
-
-// console.log(student.fullname())
-// student.sayHello()
-student.showFields()
+articles.forEach(article => {
+	console.log(article.title);
+	console.log(article.description);
+	console.log("Views: " + article.views);
+	console.log("----------------------------")
+});
