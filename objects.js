77eee9ff17b868d79b5137c28356725fb77f87ae
Mehran Tarif <mrmntf@riseup.net>
2020-12-17 11:30:39 +0330

    files

--- objects.js
+++ objects.js
@@ -6,5 +6,22 @@ let student = {
 	status: true,
 }
 
-// 0 => "programming"
-// firstname => "Mehran"
+// console.log(student)
+// console.log(student.firstname)
+// console.log(student.lastname)
+// console.log(student.age)
+// console.log(student.fields)
+// console.log(student.fields.length)
+// console.log(student.status)
+
+// let key = 'fields';
+// console.log(student[key])
+// console.log(student.key)
+
+console.log(student.firstname)
+
+student.firstname = "Ali"
+// console.log(student)
+
+student['age'] = 30
+console.log(student)
