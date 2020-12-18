edca16cdffaf49b19fcdae1a550c864beb44578a
Mehran Tarif <mrmntf@riseup.net>
2020-12-18 13:21:34 +0330

    files

---
+++ DOM.js
@@ -0,0 +1,14 @@
+// let header = document.getElementById("header")
+
+// console.log(header)
+// header.style.color = 'red'
+
+// let content = document.getElementsByClassName('content')
+
+// console.log(content[1])
+// content[0].style.fontSize = '25px'
+
+let paragraphs = document.getElementsByTagName("p")
+// console.log(paragraphs)
+// console.log(paragraphs[0].innerText)
+paragraphs[0].innerText = "Edited: Paragraph Edited by JS!"
