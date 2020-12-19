243cfe511bd69a57001981034a67362e7dd146c1
Mehran Tarif <mrmntf@riseup.net>
2020-12-19 11:39:40 +0330

    files

--- DOM.js
+++ DOM.js
@@ -1,39 +1,20 @@
-// let header = document.getElementById("header")
+// let lists = document.querySelectorAll("li")
 
-// console.log(header)
-// header.style.color = 'red'
-
-let contents = document.getElementsByClassName('content')
-
-// console.log(contents[1])
-// contents[0].style.fontSize = '25px'
-
-// let paragraphs = document.getElementsByTagName("p")
-// console.log(paragraphs)
-// console.log(paragraphs[0].innerText)
-// paragraphs[0].innerText = "Edited: Paragraph Edited by JS!"
-
-// let header = document.querySelector("#header")
-// header.style.color = 'red'
-
-// let paragraph = document.querySelector("p")
-// paragraph.style.color = 'red'
-
-// let content = document.querySelector(".content")
-// content.style.color = 'red'
-
-// let paragraphs = document.querySelectorAll("p")
-// paragraphs.forEach(paragraph => {
-// 	paragraph.style.color = 'red'
+// lists.forEach(list => {
+// 	// list.innerText += " new"
+// 	// list.innerHTML += ` <span class="badge badge-danger">new</span>`
+// 	list.textContent += " new"
 // })
 
-// let contents = document.querySelectorAll(".content")
-// Array.from(contents).forEach(content => {
-// 	content.style.color = 'tomato'
-// })
+// let list = document.querySelector("li")
 
-// console.log(contents)
+// console.log(list.innerText)
+// console.log(list.textContent)
 
-let costome_query = document.querySelector("div.content:nth-child(2) > ul:nth-child(2) > li:nth-child(2)")
-costome_query.style.color = 'green'
-costome_query.style.fontSize = '20px'
+let lists = document.querySelectorAll("li")
+
+lists.forEach(list => {
+	if (list.textContent.includes(" new")) {
+		list.innerHTML += ` <span class="badge badge-danger">new</span>`
+	}
+})
