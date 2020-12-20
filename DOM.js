163f934c8330c90b1d0944dea12e706fb5d66a38
Mehran Tarif <mrmntf@riseup.net>
2020-12-20 12:41:07 +0330

    files

--- DOM.js
+++ DOM.js
@@ -6,7 +6,16 @@
 let links = document.querySelectorAll("a")
 
 links.forEach(link => {
-	href = link.getAttribute("href")
-	link.setAttribute("href", href.replace("http://", "https://"))
-	link.setAttribute("data-protocol", "SSL")
+	// let href = link.getAttribute("href")
+	// link.setAttribute("href", href.replace("http://", "https://"))
+	// link.setAttribute("data-protocol", "SSL")
+	// let href = link.href
+	// link.href = href.replace("http://", "https://")
+	// console.log(link.dataset.protocol)
+	// console.log(href)
+	// link.dataset.protocol2 = "SSL"
+
+	// link.setAttribute("style", link.getAttribute("style") + "color: red; font-size: 20px")
+	link.style.color = 'red'
+	link.style.fontSize = '20px'
 })
