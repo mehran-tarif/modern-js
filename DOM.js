21bc377e81cc0d5b46281ce57f89b93209e95204
Mehran Tarif <mrmntf@riseup.net>
2020-12-20 12:55:56 +0330

    files

--- DOM.js
+++ DOM.js
@@ -1,21 +1,16 @@
 // let header = document.querySelector("#header")
 
-// console.log(header.getAttribute("id"))
-// header.setAttribute("title", "My header title!")
+// header.classList.add("mb-5")
+// header.classList.remove("btn-success")
+// header.classList.toggle("btn-danger")
 
-let links = document.querySelectorAll("a")
+let lists = document.querySelectorAll("li")
 
-links.forEach(link => {
-	// let href = link.getAttribute("href")
-	// link.setAttribute("href", href.replace("http://", "https://"))
-	// link.setAttribute("data-protocol", "SSL")
-	// let href = link.href
-	// link.href = href.replace("http://", "https://")
-	// console.log(link.dataset.protocol)
-	// console.log(href)
-	// link.dataset.protocol2 = "SSL"
-
-	// link.setAttribute("style", link.getAttribute("style") + "color: red; font-size: 20px")
-	link.style.color = 'red'
-	link.style.fontSize = '20px'
+lists.forEach(list => {
+	if (list.textContent.includes("success")) {
+		list.classList.add('text-success')
+	}
+	if (list.textContent.includes("danger")) {
+		list.classList.add('text-danger')
+	}
 })
