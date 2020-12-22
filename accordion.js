8428a0d783eeca765907a20821a322cd92754839
Mehran Tarif <mrmntf@riseup.net>
2020-12-22 11:57:43 +0330

    files

--- accordion.js
+++ accordion.js
@@ -1,24 +1,25 @@
-let accordion = document.querySelector(".accordion")
-
-Array.from(accordion.children).forEach(wrapper => {
-	if (wrapper.classList.contains("show")) {
-		wrapper.querySelector("div").style.maxHeight = wrapper.querySelector("div").scrollHeight + 30 + 'px'
-	}
-	wrapper.querySelector("span").addEventListener("click", e => {
-		let span = e.target
-		let wrapper = span.parentElement
-		let div = span.nextElementSibling
-		wrapper.classList.toggle("show")
+let accordions = document.querySelectorAll(".accordion")
+accordions.forEach(accordion => {
+	Array.from(accordion.children).forEach(wrapper => {
 		if (wrapper.classList.contains("show")) {
-			div.style.maxHeight = div.scrollHeight + 30 + 'px'
-		} else {
-			div.style.maxHeight = null
+			wrapper.querySelector("div").style.maxHeight = wrapper.querySelector("div").scrollHeight + 30 + 'px'
 		}
-		Array.from(accordion.children).forEach(w => {
-			if (w != wrapper) {
-				w.classList.remove("show")
-				w.querySelector('div').style.maxHeight = null
+		wrapper.querySelector("span").addEventListener("click", e => {
+			let span = e.target
+			let wrapper = span.parentElement
+			let div = span.nextElementSibling
+			wrapper.classList.toggle("show")
+			if (wrapper.classList.contains("show")) {
+				div.style.maxHeight = div.scrollHeight + 30 + 'px'
+			} else {
+				div.style.maxHeight = null
 			}
+			Array.from(accordion.children).forEach(w => {
+				if (w != wrapper) {
+					w.classList.remove("show")
+					w.querySelector('div').style.maxHeight = null
+				}
+			})
 		})
 	})
 })
