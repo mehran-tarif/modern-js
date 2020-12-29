e4437358a47452cdd3da6c25060b4ecfd2bae1bb
Mehran Tarif <mrmntf@riseup.net>
2020-12-29 14:28:38 +0330

    files

--- validation.js
+++ validation.js
@@ -1,5 +1,17 @@
-let form = document.querySelector(".form")
+let form 		= document.querySelector(".form")
+let username 	= document.querySelector("#username")
+let password 	= document.querySelector("#password")
+let seePassword 	= document.querySelector("#see-password")
 
 form.addEventListener("submit", e => {
 	e.preventDefault()
+})
+
+form.username.addEventListener("keyup", e => {
+	username.textContent = e.target.value.toLowerCase()
+})
+
+form.password.addEventListener("keyup", e => {
+	password.textContent = "*".repeat(e.target.value.length)
+	seePassword.textContent = e.target.value
 })
