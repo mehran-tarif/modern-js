e15d4fcbcd23d2848c6449f3521b37dad24f4021
Mehran Tarif <mrmntf@riseup.net>
2020-12-30 11:52:31 +0330

    files

--- validation.js
+++ validation.js
@@ -1,17 +1,35 @@
 let form 		= document.querySelector(".form")
 let username 	= document.querySelector("#username")
 let password 	= document.querySelector("#password")
-let seePassword 	= document.querySelector("#see-password")
+let seePassword = document.querySelector("#see-password")
+
+// userPatter => up
+let up = /^[a-zA-Z][\w._]{5,23}$/
 
 form.addEventListener("submit", e => {
 	e.preventDefault()
 })
 
 form.username.addEventListener("keyup", e => {
-	username.textContent = e.target.value.toLowerCase()
+	if (e.target.value) {
+		username.textContent = e.target.value.toLowerCase()
+		if (up.test(e.target.value)) {
+			e.target.classList.add("is-valid")
+			e.target.classList.remove("is-invalid")
+		} else {
+			e.target.classList.add("is-invalid")
+		}
+	} else {
+		username.innerHTML = '<i>Please write something</i>'
+	}
 })
 
 form.password.addEventListener("keyup", e => {
-	password.textContent = "*".repeat(e.target.value.length)
-	seePassword.textContent = e.target.value
+	if (e.target.value) {
+		password.textContent = "*".repeat(e.target.value.length)
+		seePassword.textContent = e.target.value
+	} else {
+		password.innerHTML = "<i>Please select a password</i>"
+		seePassword.innerHTML = "<i>Please select a password</i>"
+	}
 })
