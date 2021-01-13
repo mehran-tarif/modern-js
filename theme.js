47829b5acbe6c1471be8c12d77f29d875589f33c
Mehran Tarif <mrmntf@riseup.net>
2021-01-13 16:50:53 +0330

    files

--- theme.js
+++ theme.js
@@ -1,10 +1,15 @@
 let themes = document.querySelector(".themes")
 let selection = document.querySelector("#selection")
 
+let selectedTheme = localStorage.getItem("theme") ? localStorage.getItem("theme") : "dark"
+selection.textContent = selectedTheme
+document.body.className = selectedTheme
+
 Array.from(themes.children).forEach(theme => {
 	theme.addEventListener("click", e => {
 		let color = e.target.dataset.color
 		document.body.className = color
 		selection.textContent = color
+		localStorage.setItem("theme", color)
 	})
 })
