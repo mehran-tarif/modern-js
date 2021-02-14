a46ef94b03a00ed08d07d7858b33aacb208aecb8
Mehran Tarif <mrmntf@gmail.com>
2021-02-14 15:09:22 +0330

    files

--- todos.js
+++ todos.js
@@ -18,3 +18,30 @@ if (!todos) {
 	]
 	localStorage.setItem("todos", JSON.stringify(todos))
 }
+
+// func to create or update todos list in ui
+function createTodos(todos) {
+	let todosList = document.querySelector("#todos-list")
+	todosList.innerHTML = ""
+
+	// create list tag for each todo
+	todos.forEach(todo => {
+		let li = document.createElement("li")
+		li.className = "list-group-item"
+		let content = document.createElement("span")
+		content.textContent = todo
+		let deleteBtn = document.createElement("img")
+		deleteBtn.src = "media/delete.png"
+		deleteBtn.alt = "delete icon"
+		deleteBtn.className = 'float-right'
+
+		// append content and deleteBtn to li
+		li.append(content)
+		li.append(deleteBtn)
+
+		// append li to todosList
+		todosList.append(li)
+	});
+}
+
+createTodos(todos)
