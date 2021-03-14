6774439de2b6b14d7a0b50381f76b4edf1264948
Mehran Tarif <mrmntf@gmail.com>
2021-03-14 07:28:16 +0100

    part 10 & 11

---
+++ clock.js
@@ -0,0 +1,11 @@
+let mytime = new Date()
+
+console.log(`Date: ${mytime.getDate()}`)
+console.log(`Day: ${mytime.getDay()}`)
+console.log(`FullYear: ${mytime.getFullYear()}`)
+console.log(`Hours: ${mytime.getHours()}`)
+console.log(`Milliseconds: ${mytime.getMilliseconds()}`)
+console.log(`Minutes: ${mytime.getMinutes()}`)
+console.log(`Month: ${mytime.getMonth()}`)
+console.log(`Seconds: ${mytime.getSeconds()}`)
+console.log(`Time: ${mytime.getTime()}`)
