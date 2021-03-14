88c9b26a2c47ca2a29bd252b4d6837fd17c7e03c
Mehran Tarif <mrmntf@gmail.com>
2021-03-14 08:01:07 +0100

    part 98

--- clock.js
+++ clock.js
@@ -1,9 +1,8 @@
-let myClock = () => {
-	let clock = document.querySelector("#clock")
-	let myDate = new Date()
-	clock.textContent = `${myDate.getHours()}:${myDate.getMinutes()}:${myDate.getSeconds()}`
-}
-
-myClock()
-
-setInterval(myClock, 1000)
+let clock = document.querySelector("#clock")
+let myDate = new Date()
+let myJDate = gregorian_to_jalali(
+	myDate.getFullYear(),
+	myDate.getMonth() + 1,
+	myDate.getDate()
+)
+clock.textContent = `امسال سال ${myJDate[0]} است. امروز ماه ${myJDate[1]} و روز ${myJDate[2]} است`
