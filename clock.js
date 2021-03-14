2c1b14d3ef04840d2547057f79d5b6d07679f758
Mehran Tarif <mrmntf@gmail.com>
2021-03-14 07:40:30 +0100

    part 97

--- clock.js
+++ clock.js
@@ -1,12 +1,9 @@
-let mytime = new Date()
+let myClock = () => {
+	let clock = document.querySelector("#clock")
+	let myDate = new Date()
+	clock.textContent = `${myDate.getHours()}:${myDate.getMinutes()}:${myDate.getSeconds()}`
+}
 
+myClock()
 
-console.log(`Date: ${mytime.getDate()}`)
-console.log(`Day: ${mytime.getDay()}`)
-console.log(`FullYear: ${mytime.getFullYear()}`)
-console.log(`Hours: ${mytime.getHours()}`)
-console.log(`Milliseconds: ${mytime.getMilliseconds()}`)
-console.log(`Minutes: ${mytime.getMinutes()}`)
-console.log(`Month: ${mytime.getMonth()}`)
-console.log(`Seconds: ${mytime.getSeconds()}`)
-console.log(`Time: ${mytime.getTime()}`)
+setInterval(myClock, 1000)
