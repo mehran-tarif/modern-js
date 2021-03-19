52d9b7ac49b16ef03b755ec54d2eb6cdfb132421
Mehran Tarif <mrmntf@gmail.com>
2021-03-19 09:23:07 +0100

    part 99

--- clock.js
+++
@@ -1,8 +0,0 @@
-let clock = document.querySelector("#clock")
-let myDate = new Date()
-let myJDate = gregorian_to_jalali(
-	myDate.getFullYear(),
-	myDate.getMonth() + 1,
-	myDate.getDate()
-)
-clock.textContent = `امسال سال ${myJDate[0]} است. امروز ماه ${myJDate[1]} و روز ${myJDate[2]} است`
